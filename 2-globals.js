ad1e13931e798f039e03596d449d7de3e6a4725d
Rachel Pinto <annerachelpinto@gmail.com>
2021-08-18 17:14:15 +0530

    first commit

---
+++ 2-globals.js
@@ -0,0 +1,10 @@
+//GLOBALS
+
+//__dirname  - path to current directory
+//__filename - file name
+
+
+console.log(__dirname)
+setInterval(()=>{
+    console.log('hello world')
+}, 1000)
---
+++ 3-modules.js
@@ -0,0 +1,13 @@
+
+//modules
+//Every file in node is a module
+const names = require('./4-names');
+
+
+const sayHi = require('./5-utils');
+const data = require('./6-alternative-flavours');
+require('./7-mind-grenade')
+
+sayHi('susan');
+sayHi(names.john);
+sayHi(names.peter)
